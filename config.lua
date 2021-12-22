f50a8af586120caddab01aa4475a558d6a660dc7
KYLE S <79766794+418Gaming@users.noreply.github.com>
2021-12-22 10:32:10 -0500

    Changed percent

--- config.lua
+++ config.lua
@@ -1,15 +1,18 @@
 Crypto = {
 
     History = {
-        ["qbit"] = {}
+        ["qbit"] = {},
+        ["ubit"] = {}
     },
 
     Worth = {
-        ["qbit"] = 0
+        ["qbit"] = 0,
+        ["ubit"] = 0
     },
 
     Labels = {
-        ["qbit"] = "Qbit"
+        ["qbit"] = "Qbit",
+        ["ubit"] = "Ubit"
     },
 
     Exchange = {
@@ -21,16 +24,16 @@ Crypto = {
     },
 
     -- For auto updating the value of qbit
-    Coin = 'qbit',
-    RefreshTimer = 10, -- In minutes, so every 10 minutes.
+    Coin = 'qbit', 'ubit',
+    RefreshTimer = 60, -- In minutes, so every 10 minutes.
 
     -- Crashes or luck
     ChanceOfCrashOrLuck = 50, -- This is in % (1-100)
-    Crash = {20,80}, -- Min / Max
+    Crash = {70,30}, -- Min / Max
     Luck = {20,50}, -- Min / Max
 
     -- If not not Chance of crash or luck, then this shit
-    ChanceOfDown = 50, -- If out of 100 hits less or equal to
+    ChanceOfDown = 90, -- If out of 100 hits less or equal to
     ChanceOfUp = 50, -- If out of 100 is greater or equal to
     CasualDown = {1,10}, -- Min / Max (If it goes down)
     CasualUp = {1,10}, -- Min / Max (If it goes up)
